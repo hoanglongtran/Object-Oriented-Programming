fdec4f4aa373e08d59156da0cb4be09db84a6c71
neX- <hoanglongtran0698@gmail.com>
2017-10-27 14:50:20 +0700

    Mazen version

--- src/Class/Lab02Ex3.java
+++ src/Class/Lab02Ex3.java
@@ -1,16 +1,46 @@
 package Class;
-import java.util.*;
+
+import java.util.Scanner;
 
 public class Lab02Ex3 {
+    private static String sep = " ";
+
+    private static String printPyramid(int n) {
+        String output = "";
+        for (int i=1; i<n+1; i++)
+            output += spaceIt(n-i) + symmetricalize(i) + spaceIt(n-i) + "\n";
+        return output;
+    }
+
+    /**
+     * give n, returns : n n-1 n-2 ... n-2 n-1 n (e.g. give 3, return "3 2 1 2 3")
+     */
+    private static String symmetricalize(int i) {
+        String result = "";
+        int start = i;          //save the number
+        while (i>0)             //keep decrementing until 1 and adding separator
+            result += i-- + sep;
+        i++;                    // from 0 skip 1
+        while (++i<=start)      //keep incrementing and adding separator
+            result += i + sep;
+        return result;
+    }
+
+    /**
+     * give n, returns : n*(" "+sep)
+     */
+    private static String spaceIt(int n) {
+        String result = "";
+        while (n-->0)
+            result += " "+sep;
+        return result;
+    }
+
     public static void main(String[] args) {
-        System.out.print("Enter the number of line: ");
-        Scanner input = new Scanner(System.in);
-        int s1 = input.nextInt();
-        int a=0;
-
-        for(a = 1, a<=s1, a++){
-            System.out.printf("");;
-        }
-
+        Scanner s = new Scanner(System.in);
+        System.out.println("Please insert input number :");
+        int input = s.nextInt();
+        System.out.println(printPyramid(input));
+        s.close();
     }
 }
