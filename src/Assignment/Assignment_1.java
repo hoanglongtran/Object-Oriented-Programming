7735ddfd57bec7eafc4b3a3fb7111187c2fb7d87
neX- <hoanglongtran0698@gmail.com>
2017-11-03 10:15:35 +0700

    assignment 1

--- src/Assignment/Assignment_1.java
+++ src/Assignment/Assignment_1.java
@@ -6,11 +6,16 @@ public class Assignment_1 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
-        while () {
-            System.out.printf("Enter the string: ");
+        while (true) {
+            System.out.print("Enter the string: ");
             String input = in.next();
             int len = input.length();
+            if (input.equals("-1")) {
+                System.out.println("Exit code detected! Exit the program");
+                break;
+            }
             if (check(input, len)) {
+                System.out.printf("Sorted string: ");
                 System.out.println(sortAlphabetical(input));
             }
         }
@@ -19,15 +24,13 @@ public class Assignment_1 {
 
     private static boolean check(String input, int length) {
 
-        for (int n = 0; n <= length; n++) {
+        for (int n = 0; n < length; n++) {
             if (!Character.isLetter(input.charAt(n))) {
-                System.out.println("Please insert letters only");
+                System.out.println("Invalid input! Please insert letters only");
                 return false;
-            } else {
-                return true;
             }
         }
-        return false;
+        return true;
     }
 
     private static String sortAlphabetical(String n) {
@@ -46,7 +49,6 @@ public class Assignment_1 {
             }
 
         }
-        String result = new String(input);
-        return result;
+        return new String(input);
     }
 }
